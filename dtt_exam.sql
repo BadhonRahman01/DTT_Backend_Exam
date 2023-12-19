dee8d56f0779ed852ee4f2c163f38642cb91da0c
BadhonRahman01 <badhon.rahman184@gmail.com>
2023-12-19 20:00:41 +0200

    US2 and US3 Done

---
+++ dtt_exam.sql
@@ -0,0 +1,186 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Dec 19, 2023 at 06:37 PM
+-- Server version: 10.4.25-MariaDB
+-- PHP Version: 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `dtt_exam`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `facility`
+--
+
+CREATE TABLE `facility` (
+  `facility_id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `creation_date` date NOT NULL,
+  `location_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `facility`
+--
+
+INSERT INTO `facility` (`facility_id`, `name`, `creation_date`, `location_id`) VALUES
+(5, 'Sydney Facility', '2023-12-19', 1),
+(6, 'Helsinki Facility', '2023-12-19', 3),
+(7, 'Amsterdam Facility', '2023-12-18', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `facilitytags`
+--
+
+CREATE TABLE `facilitytags` (
+  `facility_id` int(11) NOT NULL,
+  `tag_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `facilitytags`
+--
+
+INSERT INTO `facilitytags` (`facility_id`, `tag_id`) VALUES
+(5, 3),
+(6, 1),
+(6, 2),
+(6, 3),
+(7, 2),
+(7, 3);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `location`
+--
+
+CREATE TABLE `location` (
+  `location_id` int(11) NOT NULL,
+  `city` varchar(255) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `zip_code` varchar(20) NOT NULL,
+  `country_code` varchar(10) NOT NULL,
+  `phone_number` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `location`
+--
+
+INSERT INTO `location` (`location_id`, `city`, `address`, `zip_code`, `country_code`, `phone_number`) VALUES
+(1, 'Sydney', '575 Sydney, Barkley Road', '1655', 'AUS', '01950498055'),
+(2, 'Amasterdam', '36 Amsterdam Road', '1884', 'NED', '01974684658'),
+(3, 'Helsinki', '5 C Punkerikatu', '58380', 'FIN', '01465789544');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tag`
+--
+
+CREATE TABLE `tag` (
+  `tag_id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tag`
+--
+
+INSERT INTO `tag` (`tag_id`, `name`) VALUES
+(2, 'Business'),
+(3, 'Finance'),
+(1, 'IT');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `facility`
+--
+ALTER TABLE `facility`
+  ADD PRIMARY KEY (`facility_id`),
+  ADD KEY `location_id` (`location_id`);
+
+--
+-- Indexes for table `facilitytags`
+--
+ALTER TABLE `facilitytags`
+  ADD PRIMARY KEY (`facility_id`,`tag_id`),
+  ADD KEY `tag_id` (`tag_id`);
+
+--
+-- Indexes for table `location`
+--
+ALTER TABLE `location`
+  ADD PRIMARY KEY (`location_id`);
+
+--
+-- Indexes for table `tag`
+--
+ALTER TABLE `tag`
+  ADD PRIMARY KEY (`tag_id`),
+  ADD UNIQUE KEY `name` (`name`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `facility`
+--
+ALTER TABLE `facility`
+  MODIFY `facility_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `location`
+--
+ALTER TABLE `location`
+  MODIFY `location_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `tag`
+--
+ALTER TABLE `tag`
+  MODIFY `tag_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `facility`
+--
+ALTER TABLE `facility`
+  ADD CONSTRAINT `facility_ibfk_1` FOREIGN KEY (`location_id`) REFERENCES `location` (`location_id`);
+
+--
+-- Constraints for table `facilitytags`
+--
+ALTER TABLE `facilitytags`
+  ADD CONSTRAINT `facilitytags_ibfk_1` FOREIGN KEY (`facility_id`) REFERENCES `facility` (`facility_id`),
+  ADD CONSTRAINT `facilitytags_ibfk_2` FOREIGN KEY (`tag_id`) REFERENCES `tag` (`tag_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- facility/read.php
+++ facility/read.php
@@ -11,6 +11,72 @@ $conn = new mysqli($host, $user, $pass, $db);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
+
+if ($_SERVER['REQUEST_METHOD'] === 'GET' && isset($_GET['search'])) {
+    $search = $_GET['search'];
+
+    // Pagination parameters
+    $page = isset($_GET['page']) ? intval($_GET['page']) : 1;
+    $pageSize = 5; // Number of results per page
+
+    // Calculate offset for traditional pagination
+    $offset = ($page - 1) * $pageSize;
+
+    // Search facilities by facility name, tag name, or location city with pagination
+    $query = "SELECT Facility.*, Location.*, GROUP_CONCAT(DISTINCT Tag.name) AS tag_names
+              FROM Facility
+              LEFT JOIN Location ON Facility.location_id = Location.location_id
+              LEFT JOIN FacilityTags ON Facility.facility_id = FacilityTags.facility_id
+              LEFT JOIN Tag ON FacilityTags.tag_id = Tag.tag_id
+              WHERE Facility.name LIKE '%$search%'
+                 OR Tag.name LIKE '%$search%'
+                 OR Location.city LIKE '%$search%'
+              GROUP BY Facility.facility_id
+              LIMIT $offset, $pageSize";
+
+    $result = $conn->query($query);
+
+    if ($result->num_rows > 0) {
+        $facilities = [];
+        while ($row = $result->fetch_assoc()) {
+            // Combine results
+            $location = [
+                'city' => $row['city'],
+                'address' => $row['address'],
+                'zip_code' => $row['zip_code'],
+                'country_code' => $row['country_code'],
+                'phone_number' => $row['phone_number']
+            ];
+
+            $tags = !empty($row['tag_names']) ? explode(',', $row['tag_names']) : [];
+
+            $row['location'] = $location;
+            $row['tags'] = $tags;
+
+            $facilities[] = $row;
+        }
+
+        // Calculate the next cursor for cursor pagination
+        $nextCursor = null;
+        if ($result->num_rows === $pageSize) {
+            $lastFacility = end($facilities);
+            $nextCursor = base64_encode(json_encode(['facility_id' => $lastFacility['facility_id']]));
+        }
+
+        // Response data
+        $response = [
+            'facilities' => $facilities,
+            'next_cursor' => $nextCursor
+        ];
+
+        echo json_encode($response);
+    } else {
+        // Handle error: No matching facilities found
+        echo json_encode(["error" => "No matching facilities found"]);
+    }
+}
+
+
 // Read one facility, its location, and its tags
 if ($_SERVER['REQUEST_METHOD'] === 'GET') {
     if (isset($_GET['facility_id'])) {
